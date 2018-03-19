88c9967858cf73060594d57178e6bee56a7470e9
James Tease <james@jamestease.co.uk>
2018-03-19 17:25:16 +0000

    first pass

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ use rand::distributions::{ IndependentSample, Range };
 use cairo::{ ImageSurface, Format, Context };
 use std::fs::File;
 
+const MARGIN: f64 = 5.0;
+
+#[derive(Debug)]
 struct Circle {
     colour: (f64, f64, f64),
     x: f64,
@@ -37,11 +40,35 @@ impl Circle {
         context.arc(self.x, self.y, self.radius, 0.0, 360.0);
         context.fill();
     }
+
+    fn collides(&self, other: &Circle) -> bool {
+        dist(self.x, self.y, other.x, other.y) < (self.radius + other.radius + MARGIN)
+    }
+}
+
+fn is_valid(circle: &Circle, circles: &Vec<Circle>) -> bool {
+    // width / 2, height / 2
+    if dist(circle.x, circle.y, 300.0, 200.0) > 200.0 {
+        return false;
+    }
+
+    for c in circles {
+        if circle.collides(c) {
+            return false;
+        }
+    }
+    true
+}
+
+fn dist(x1: f64, y1: f64, x2: f64, y2: f64) -> f64 {
+    ((x2 - x1).powi(2) + (y2 - y1).powi(2)).sqrt()
 }
 
 fn main() {
     let width = 600;
     let height = 400;
+    let mut radius = 16.0;
+    let RADIUS_MIN = 2.0;
 
     let surface = ImageSurface::create(Format::ARgb32, width, height)
         .expect("Couldn't create surface");
@@ -49,17 +76,34 @@ fn main() {
     let mut circles: Vec<Circle> = vec![];
     let mut rng = rand::thread_rng();
 
-    for _ in 0..100 {
+    let mut failed_tries = 0;
+
+    for _ in 0..20000 {
+        // if failed_tries < 32 * 1024 / radius {
         let between = Range::new(0.0, width as f64);
         let between_y = Range::new(0.0, height as f64);
 
         let x = between.ind_sample(&mut rng);
         let y = between_y.ind_sample(&mut rng);
 
-        let circle = Circle::new(x, y, 15.0);
-        circles.push(circle);
+        let circle = Circle::new(x, y, radius);
+        if is_valid(&circle, &circles) {
+            circles.push(circle);
+            // }
+        } else {
+            failed_tries += 1;
+            if failed_tries as f64 > (32 * 1024) as f64 / radius {
+                radius /= 2.0;
+                failed_tries = 0;
+
+                if radius < RADIUS_MIN {
+                    break;
+                }
+            }
+        }
     }
 
+    println!("{:?}", circles.len());
     for circle in circles {
         circle.draw(&context);
     }
